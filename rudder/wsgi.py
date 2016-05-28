4205faff38f2bfa25427fab33bebf75da00e2ee6
cutini <joseibieta@gmail.com>
2016-05-28 03:40:36 -0300

    x

--- rudder/wsgi.py
+++ rudder/wsgi.py
@@ -14,6 +14,6 @@ import os, sys
 from django.core.wsgi import get_wsgi_application
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gsimple.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rudder.settings")
 
 application = get_wsgi_application()
