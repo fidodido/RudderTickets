4dfa100dc29c453d7bd949ddadb6347903db1beb
cutini <joseibieta@gmail.com>
2016-05-28 03:43:12 -0300

    x

--- ticket/admin.py
+++ ticket/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+# Register your models here.
+from app.models import Project, Type, Status, Ticket, UserDetail, Comment, Action, Workflow
+
+# Register your models here.
+# Register your models here.
+admin.site.register(Project)
+admin.site.register(Type)
+admin.site.register(Status)
+admin.site.register(Ticket)
+admin.site.register(UserDetail)
+admin.site.register(Comment)
+admin.site.register(Action)
+admin.site.register(Workflow)
